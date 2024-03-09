ce2871ec5292332bb5bccf26215d3c5a399777de
Col-lin <3078046011@qq.com>
2024-03-09 15:27:25 +0800

    lab1_ch3 finished

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -10,6 +10,7 @@
 #include "string.h"
 #include "process.h"
 #include "util/functions.h"
+#include "sync_utils.h"
 
 #include "spike_interface/spike_utils.h"
 
@@ -17,19 +18,29 @@
 // implement the SYS_user_print syscall
 //
 ssize_t sys_user_print(const char* buf, size_t n) {
-  sprint("hartid = ?: %s\n", buf);
+    int hart_id = read_tp();
+//  sprint("hartid = ?: %s\n", buf);
+    sprint("hartid = %d: %s", hart_id, buf);
   return 0;
 }
 
 //
 // implement the SYS_user_exit syscall
 //
+static volatile int exit_counter = 0;
 ssize_t sys_user_exit(uint64 code) {
-  sprint("hartid = ?: User exit with code:%d.\n", code);
+    int hart_id = read_tp();
+//  sprint("hartid = ?: User exit with code:%d.\n", code);
+    sprint("hartid = %d: User exit with code:%d.\n", hart_id, code);
   // in lab1, PKE considers only one app (one process). 
   // therefore, shutdown the system when the app calls exit()
-  sprint("hartid = ?: shutdown with code:%d.\n", code);
-  shutdown(code);
+//  sprint("hartid = ?: shutdown with code:%d.\n", code);
+    sync_barrier(&exit_counter, NCPU);
+    if (hart_id == 0) {
+        sprint("hartid = %d: shutdown with code:%d.\n", hart_id, code);
+        shutdown(code);
+    }
+    return 0;
 }
 
 //
