34255b5ad494205b4fd1efa2b195389bd5d80ded
Col-lin <3078046011@qq.com>
2024-03-10 21:27:54 +0800

    lab2_3 done

--- kernel/strap.c
+++ kernel/strap.c
@@ -61,7 +61,14 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
       // dynamically increase application stack.
       // hint: first allocate a new physical page, and then, maps the new page to the
       // virtual address that causes the page fault.
-      panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
+//      panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
+        uint64 pa;
+        pa = (uint64) alloc_page();
+        if ((void*)pa == NULL)
+            sprint("failed to allocate a new physical page\n");
+        else {
+            map_pages(current->pagetable, ROUNDDOWN(stval, PGSIZE), PGSIZE, pa, prot_to_type(PROT_READ | PROT_WRITE, 1));
+        }
 
       break;
     default:
